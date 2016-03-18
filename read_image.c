aa862dea27b52b59d80977646f85a2395ad87da5
camboui <mathias.branger@gmail.com>
2016-03-18 12:08:34 +0100

    add normalise

--- read_image.c
+++ read_image.c
@@ -9,18 +9,29 @@
 #define WIDTH (SIZE*SIZE*SIZE)
 #define COL 4 //le nombre de valeurs à afficher avant un retour à la ligne
 
-void init( int cube[],int length);
-void colorSection(int cube[], CIMAGE cim);
-void printCube(int cube[],int nbCol,FILE*f);
+void init( float cube[],int length);
+void colorSection(float cube[], CIMAGE cim);
+void printCube(float cube[],int nbCol,FILE*f);
 void createHistograms(char * fileToRead);
 void processOneFile(char * stringIn);
-
+void normalise(float cube[],float nbPixel);
 
 int main(int argc, char *argv[])
 {
 	createHistograms("img/list.txt");
 	exit(0);
 }
+
+//normalise le cube pour transformer les valeurs entre 0 et 1
+void normalise(float cube[],float nbPixel)
+{ 
+	for (int i = 0; i < WIDTH; i++) 
+	{      
+		cube[i]/=nbPixel;
+	}
+}
+
+
 
 //construit la totalité des histogrammes à partir d'un fichier contenant une liste de noms de fichiers
 void createHistograms(char * fileToRead)
@@ -51,7 +62,7 @@ void processOneFile(char * stringIn)
 	CIMAGE cim;
 	char toRead[100]="img/images/";
 	char toCreate[100]="img/hist/";
-	int cube[WIDTH];
+	float cube[WIDTH];
 	init(cube,WIDTH);
 	
 	strcat(toCreate,stringIn);
@@ -65,18 +76,19 @@ void processOneFile(char * stringIn)
 	strcat(toRead,stringIn);
 	read_cimage(toRead,&cim);
 	
-	colorSection(cube,cim);
-	printCube(cube,COL,out);
+	colorSection(cube,cim); // Construit le cube
+	normalise(cube,cim.nx*cim.ny); // Normalise le cube 
+	printCube(cube,COL,out); // Ecrit le contenu du cube dans le fichier
 	
 	fclose(out);
 }
 
 //Affiche le contenu d'un histogramme avec un saut de ligne toutes les nbCol multiples
-void printCube(int cube[],int nbCol,FILE*f)
+void printCube(float cube[],int nbCol,FILE*f)
 {
    for (int j = 0; j < WIDTH; j++)
    {
-   		fprintf(f,"%i ",cube[j]);
+   		fprintf(f,"%f ",cube[j]);
    		if(j!=0 && (j+1)%nbCol==0)
    		{
    			fprintf(f,"\n");
@@ -85,7 +97,7 @@ void printCube(int cube[],int nbCol,FILE*f)
 }
 
 //Initialise un histogramme en le remplissant de 0
-void init( int cube[],int length)
+void init(float cube[],int length)
 {
    for (int j = 0; j < length; j++)
    {
@@ -94,7 +106,7 @@ void init( int cube[],int length)
 }
 
 //Rempli le contenu d'un histogramme à partir d'une CIMAGE
-void colorSection(int cube[], CIMAGE cim)
+void colorSection(float cube[], CIMAGE cim)
 {
 	int k=0;
 	for (int i = 0; i < cim.nx; i++) 
